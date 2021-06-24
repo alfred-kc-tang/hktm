ee02689f32b0378b04accefefaa5edeb84c1bca2
Alfred Tang <kctang@link.cuhk.edu.hk>
2021-06-24 13:46:47 +0800

    Third commit

--- models.py
+++ models.py
@@ -5,13 +5,14 @@ from sqlalchemy import Column, String, Integer, ForeignKey, create_engine
 from flask_sqlalchemy import SQLAlchemy
 
 # Connect the local database
-database_name = "hktm"
-database_path = "postgresql://{}/{}".format('alfred.kctang@localhost:5432', database_name)
+# database_name = "hktm"
+# database_path = "postgresql://{}/{}".format('alfred.kctang@localhost:5432', database_name)
+database_path = os.environ['DATABASE_URL']
 
 db = SQLAlchemy()
 
 def setup_db(app, database_path=database_path):
-	app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL')
+	app.config["SQLALCHEMY_DATABASE_URI"] = database_path
 	app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 	db.app = app
 	db.init_app(app)
